cf240a90f7ab7380e55f5bdfd3635ec2522ffa75
liubov_butorina <liubovbutorina7@gmail.com>
2021-02-18 15:34:22 +0300

    add js-file

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+
+  return userName;
+};
+
+export default getUserName;
