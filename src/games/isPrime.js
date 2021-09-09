0545bbc8684708acfa87a86303c9e05a6129bc00
liubovbutorina <liubovbutorina7@gmail.com>
2021-09-09 19:43:27 +0300

    add import of function

--- src/games/isPrime.js
+++ src/games/isPrime.js
@@ -1,8 +1,9 @@
-const getExpression = () => {
+/* const getExpression = () => {
   const value = Math.floor(Math.random() * 100) + 1;
   const expression = `${value}`;
   return [value, expression];
-};
+}; */
+import getExpression from '../functions/functions.js';
 
 const getCorrectAnswer = (value) => {
   if (value === 1) {
